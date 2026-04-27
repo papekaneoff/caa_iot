531678b4871865d2f7c82ecbb198363558256ba8
geekarim <karimalkhadzh@gmail.com>
2026-04-27 14:13:07 +0200

    add chart

---
+++ backend/core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import weather_data
+
+urlpatterns = [
+    path("weather/", weather_data),
+]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from google.cloud import bigquery
+from django.http import JsonResponse
+
+client = bigquery.Client()
+
+def weather_data(request):
+    query = """
+        SELECT
+            device_id,
+            temperature,
+            humidity,
+            pressure,
+            timestamp
+        FROM `caaproject-490112.weather.weather-data`
+        ORDER BY timestamp DESC
+        LIMIT 200
+    """
+
+    results = client.query(query).result()
+
+    data = []
+    for row in results:
+        data.append({
+            "device_id": row.device_id,
+            "temperature": row.temperature,
+            "humidity": row.humidity,
+            "pressure": row.pressure,
+            "timestamp": row.timestamp.isoformat()
+        })
+
+    return JsonResponse(data, safe=False)
