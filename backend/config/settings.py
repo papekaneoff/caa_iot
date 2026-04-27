72b3c4552251f8d9c2e8c969235b22f68f6c1346
geekarim <karimalkhadzh@gmail.com>
2026-04-27 14:26:10 +0200

    fix chart

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "https://nextjs-frontend-50284710838.europe-west1.run.app"
+]
