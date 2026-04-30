2cbec14c1fd536bfe8aab224c775393b3ebcd9bc
Kane Pape Sidiya <KanePS@s2.rpn.ch>
2026-04-30 11:12:47 +0200

    Revert "feat: add voice interaction module (STT/TTS/announcements)"
    This reverts commit 17cf6bd460ca8b5391d3d7891f97ddfea0b4cb26.

--- device/interaction/__init__.py
+++
@@ -1,7 +0,0 @@
-# device/interaction/__init__.py
-# Makes this directory a Python package.
-# Import the main controller for convenience.
-
-from .main_interaction import InteractionController
-
-__all__ = ["InteractionController"]
